44066ecd3b1c5b5e5deccb66f4ca17c86c426bdf
Daniela Romero <danyrp45@gmail.com>
2023-01-03 17:24:50 -0600

    use switchMap in ver-pais component

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PaisService } from '../../services/pais.service';
+import {switchMap} from 'rxjs/operators'
 
 @Component({
   selector: 'app-ver-pais',
@@ -13,11 +14,19 @@ export class VerPaisComponent implements OnInit {
     private paisService: PaisService
   ) {}
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe(({ id }) => {
+
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({id}) => this.paisService.getPaisPorAlpha(id))
+      )
+      .subscribe(resp =>{
+        console.log(resp)
+      })
+  /*   this.activatedRoute.params.subscribe(({ id }) => {
       console.log(id);
       this.paisService.getPaisPorAlpha(id).subscribe(pais=>{
         console.log(pais)
       })
-    });
+    }); */
   }
 }
