329411c182288061bb7b3b0a521dab4021f405de
Daniela Romero <danyrp45@gmail.com>
2023-01-03 17:11:33 -0600

    implements aplha http request

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-ver-pais',
   templateUrl: './ver-pais.component.html',
-  styleUrls: ['./ver-pais.component.css']
+  styleUrls: ['./ver-pais.component.css'],
 })
-export class VerPaisComponent {
-
+export class VerPaisComponent implements OnInit {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private paisService: PaisService
+  ) {}
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(({ id }) => {
+      console.log(id);
+      this.paisService.getPaisPorAlpha(id).subscribe(pais=>{
+        console.log(pais)
+      })
+    });
+  }
 }
