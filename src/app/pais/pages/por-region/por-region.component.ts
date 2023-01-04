59f9a47b546daf9f95a9df360d38ea020abdadbf
Daniela Romero <danyrp45@gmail.com>
2023-01-04 13:00:51 -0600

    add content (buttons) to region section

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -3,8 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
-  styleUrls: ['./por-region.component.css']
+  styles: [
+    `
+      button {
+        margin-right: 5px;
+      }
+    `,
+  ],
 })
 export class PorRegionComponent {
+  regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  regionActiva: string = '';
 
+  constructor() {}
+
+  getClaseCSS(region: string): string {
+    return region === this.regionActiva
+      ? 'btn btn-primary'
+      : ' btn btn-outline-primary';
+  }
+
+  activarRegion(region: string) {
+    this.regionActiva = region;
+    
+  }
 }
